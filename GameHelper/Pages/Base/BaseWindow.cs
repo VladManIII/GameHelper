563158b915b2d3149d7d3ffd6e7e0d56119b231e
VladManIII <valex.human@gmail.com>
2026-07-07 12:38:41 +0300

    WRD-5: formed base MVVM structure

---
+++ GameHelper/Pages/Base/BaseWindow.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System;
+using WinRT;
+
+namespace GameHelper.Pages
+{
+    public abstract class BaseWindow: Window
+    {
+        private IViewModelLifecycle? Model => GetViewModel();
+
+        public BaseWindow()
+        {
+
+            this.Closed += OnClosed;
+            this.Activated += OnActivated;
+            this.VisibilityChanged += OnVisibilityChanged;
+        }
+
+        public abstract IViewModelLifecycle? GetViewModel();
+
+        protected void OnClosed(object sender, WindowEventArgs args)
+        {
+            Model?.Closed();
+
+            this.Closed -= OnClosed;
+            this.Activated -= OnActivated;
+            this.VisibilityChanged -= OnVisibilityChanged;
+        }
+
+        protected void OnActivated(object sender, WindowActivatedEventArgs args)
+        {
+            Model?.Activated();
+        }
+
+        protected void OnVisibilityChanged(object sender, WindowVisibilityChangedEventArgs args)
+        {
+            Model?.VisibilityChanged();
+        }
+
+    }
+}
---
+++ GameHelper/Pages/Base/IViewModelLifecycle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameHelper.Pages
+{
+    public interface IViewModelLifecycle
+    {
+        void Activated();
+        void Closed();
+        void VisibilityChanged();
+    }
+}
